07f74f5dfbbf54bdad1cd7acbfe73bd6affa5754
Haik <55463651+salampego@users.noreply.github.com>
2023-05-29 21:08:59 +0300

    Update products.js

--- models/products.js
+++ models/products.js
@@ -1,20 +1,18 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  id: String,
-});
-
 const deliverySchema = new mongoose.Schema({
-  _id: {
-    type: mongoose.Schema.Types.ObjectId,
-    auto: true,
-  },
-  products: [productSchema],
-  restaurant: String,
+  products: [
+    {
+      name: { type: String },
+      price: { type: Number },
+      id: { type: String },
+    },
+  ],
+  name: { type: String },
 });
 
-const Delivery = mongoose.model("delivery_list", deliverySchema);
-console.log(Delivery);
-module.exports = { Delivery };
+const Delivery = mongoose.model("deliverys", deliverySchema);
+
+module.exports = {
+  Delivery,
+};
